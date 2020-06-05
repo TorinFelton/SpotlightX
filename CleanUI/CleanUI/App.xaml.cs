d3f41e7d3665e610f4ea356cf38c0c773afd6468
Dave Roberts <droberts@abacus.systems>
2020-06-05 16:56:59 +0100

    Refactoring through constructor

--- CleanUI/CleanUI/App.xaml.cs
+++ CleanUI/CleanUI/App.xaml.cs
@@ -1,17 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
+using CleanUI.Interfaces;
+using CleanUI.Interfaces.Implementations;
+using Unity;
+using Unity.Injection;
 
 namespace CleanUI
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
+    // ReSharper disable once RedundantExtendsListEntry
     public partial class App : Application
     {
+        private readonly string _configPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) +
+                                              @"\AppData\Roaming\Microsoft\Windows\Start Menu\SpotlightXConfig\config\";
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var settingsPath = _configPath + "settings.json";
+            var container = new UnityContainer();
+            container.RegisterSingleton<ISettingsLoader, FileSystemSettingsLoaderAndSaver>(
+                new InjectionConstructor(settingsPath));
+            container.RegisterSingleton<ISettingsSaver, FileSystemSettingsLoaderAndSaver>(
+                new InjectionConstructor(settingsPath));
+            container.RegisterSingleton<ICommandLoader, CommandLoader>();
+            container.RegisterSingleton<IProgramListLoader, StartMenuProgramListLoader>();
+            var window = container.Resolve<MainWindow>();
+
+            ShutdownMode = ShutdownMode.OnExplicitShutdown;
+            MainWindow = window;
+            Debug.Assert(MainWindow != null, nameof(MainWindow) + " != null");
+            MainWindow.Show();
+        }
     }
 }
